9891e77699a68b175288889cf3a82e0e1c44e2f8
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2023-05-13 03:01:20 +0530

    changed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    ".src/public/index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+    screens:{
+      'sm':'480px',
+      'md':'768px',
+      'lg':'976px',
+      'xl':'1440px'
+    },
     extend: {},
   },
   plugins: [],
